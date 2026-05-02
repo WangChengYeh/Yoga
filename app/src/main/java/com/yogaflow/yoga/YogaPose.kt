12b8a73501985e983c026b4d4bc35b02448c2670
WangChengYeh <cywang.eda@gmail.com>
2026-05-02 16:53:47 +0800

    Add YogaPose model

---
+++ app/src/main/java/com/yogaflow/yoga/YogaPose.kt
@@ -0,0 +1,9 @@
+package com.yogaflow.yoga
+
+data class YogaPose(
+    val id: String,
+    val displayName: String,
+    val category: String,
+    val setupCue: String,
+    val correctionFocus: String
+)
