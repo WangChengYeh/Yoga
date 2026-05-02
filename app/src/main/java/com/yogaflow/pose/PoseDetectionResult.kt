b8d88fb603e019e0d8aa53d22406b06eb1a57d5d
WangChengYeh <cywang.eda@gmail.com>
2026-05-02 20:51:41 +0800

    Add pose detection result model

---
+++ app/src/main/java/com/yogaflow/pose/PoseDetectionResult.kt
@@ -0,0 +1,11 @@
+package com.yogaflow.pose
+
+import com.google.mediapipe.tasks.components.containers.Landmark
+import com.google.mediapipe.tasks.components.containers.NormalizedLandmark
+
+data class PoseDetectionResult(
+    val imageLandmarks: List<NormalizedLandmark>,
+    val worldLandmarks: List<Landmark> = emptyList(),
+    val imageWidth: Int = 0,
+    val imageHeight: Int = 0
+)
