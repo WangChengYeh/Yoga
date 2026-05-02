e9589503349c8749f4f273e8625c57fde659e4a0
WangChengYeh <cywang.eda@gmail.com>
2026-05-03 00:36:04 +0800

    Add debug pose info model

---
+++ app/src/main/java/com/yogaflow/pose/DebugPoseInfo.kt
@@ -0,0 +1,31 @@
+package com.yogaflow.pose
+
+import com.yogaflow.coach.CoachState
+
+data class DebugPoseInfo(
+    val poseId: String,
+    val detect: String,
+    val state: CoachState,
+    val matched: Boolean,
+    val leftKneeAngle: Double?,
+    val rightKneeAngle: Double?,
+    val leftHipAngle: Double?,
+    val rightHipAngle: Double?,
+    val torsoTwistEstimate: Double?
+) {
+    fun toDisplayText(): String {
+        return buildString {
+            appendLine("DEBUG")
+            appendLine("pose=$poseId")
+            appendLine("detect=$detect")
+            appendLine("state=$state matched=$matched")
+            appendLine("L knee=${leftKneeAngle.fmt()} R knee=${rightKneeAngle.fmt()}")
+            appendLine("L hip=${leftHipAngle.fmt()} R hip=${rightHipAngle.fmt()}")
+            appendLine("twist=${torsoTwistEstimate.fmt()}")
+        }
+    }
+
+    private fun Double?.fmt(): String {
+        return this?.let { "%.1f°".format(it) } ?: "--"
+    }
+}
