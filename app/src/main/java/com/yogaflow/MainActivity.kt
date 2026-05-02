013d63cb08a0bc083c0705a320e6a5dbb7c2e3c5
WangChengYeh <cywang.eda@gmail.com>
2026-05-02 16:28:02 +0800

    Add CameraX preview + analyzer

--- app/src/main/java/com/yogaflow/MainActivity.kt
+++ app/src/main/java/com/yogaflow/MainActivity.kt
@@ -3,20 +3,59 @@ package com.yogaflow
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var previewView: PreviewView
+    private val cameraExecutor = Executors.newSingleThreadExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestCameraPermission()
+
+        previewView = findViewById(R.id.previewView)
+
+        if (checkSelfPermission(android.Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.CAMERA), 100)
+        }
     }
 
-    private fun requestCameraPermission() {
-        if (checkSelfPermission(android.Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED) {
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
-        }
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            val imageAnalyzer = ImageAnalysis.Builder().build().also {
+                it.setAnalyzer(cameraExecutor) { imageProxy ->
+                    // TODO: send frame to MediaPipe
+                    imageProxy.close()
+                }
+            }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                preview,
+                imageAnalyzer
+            )
+
+        }, mainExecutor)
     }
 }
