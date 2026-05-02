8e137ac9f2180bf8062517f31f9d2757acddd7f4
WangChengYeh <cywang.eda@gmail.com>
2026-05-02 16:25:10 +0800

    Add app module build.gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.yogaflow"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.yogaflow"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
