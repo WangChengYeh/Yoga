e2b265fd9e357986422b1be7baa82daadddb6e9a
WangChengYeh <cywang.eda@gmail.com>
2026-05-03 09:48:26 +0800

    Add fail reason to pose detection result

--- app/src/main/java/com/yogaflow/coach/PoseDetectionMappingResult.kt
+++ app/src/main/java/com/yogaflow/coach/PoseDetectionMappingResult.kt
@@ -3,5 +3,6 @@ package com.yogaflow.coach
 data class PoseDetectionMappingResult(
     val matched: Boolean,
     val state: CoachState,
-    val cue: String
+    val cue: String,
+    val reason: String = ""
 )
