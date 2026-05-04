b18bdd5af9f64cc8ce5aa35f9be88299ccd106cc
WangChengYeh <cywang.eda@gmail.com>
2026-05-05 07:15:32 +0800

    Add avatar bone abstraction

---
+++ app/src/main/java/com/yogaflow/avatar/AvatarBone.kt
@@ -0,0 +1,30 @@
+package com.yogaflow.avatar
+
+enum class AvatarBone {
+    HIPS,
+    SPINE,
+    SPINE1,
+    SPINE2,
+    NECK,
+    HEAD,
+
+    LEFT_SHOULDER,
+    LEFT_ARM,
+    LEFT_FORE_ARM,
+    LEFT_HAND,
+
+    RIGHT_SHOULDER,
+    RIGHT_ARM,
+    RIGHT_FORE_ARM,
+    RIGHT_HAND,
+
+    LEFT_UP_LEG,
+    LEFT_LEG,
+    LEFT_FOOT,
+    LEFT_TOE_BASE,
+
+    RIGHT_UP_LEG,
+    RIGHT_LEG,
+    RIGHT_FOOT,
+    RIGHT_TOE_BASE
+}
